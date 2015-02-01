fd9166aff8b04a4ab69d05d08bbad85ad70ba1f2
Kunal Mathur <kunal.mathur@tcs.com>
2015-02-01 17:48:05 +0530

    Adding JsHint Plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,16 @@
-module.exports = function (grunt) {
-	// body...
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+    all: ['Gruntfile.js']
+  }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint']);
+
 };
