e60428b126ff4a5fd2d121a65a174ec0759102bf
Kunal Mathur <mathurkunal@gmail.com>
2015-02-02 05:22:57 +0000

    adding uglify Plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,31 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/*.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
     jshint: {
-    all: ['Gruntfile.js']
+      options: {
+        unused:true,
+        
+        //reporterOutput: 'jshint.html'
+          //reporter: require('checkstyle')
+      },
+      all: ['Gruntfile.js', 'src/*.js']
   }
   });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+ require('load-grunt-tasks')(grunt);
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint','uglify']);
 
 };
