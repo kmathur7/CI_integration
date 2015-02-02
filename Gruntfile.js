42cfc7b91fda8345113b12b63223ccc1293c0b64
Kunal Mathur <mathurkunal@gmail.com>
2015-02-02 05:49:27 +0000

    Adding JsHint Options

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,16 @@ module.exports = function(grunt) {
     },
     jshint: {
       options: {
-        unused:true,
+        unused:true,      /* warns when you define and never use your variables. */
+        curly:true,       /* requires you to always put curly braces around blocks in loops and conditionals. */
+        camelcase:true,   /* force all identifiers (function, variable and property) to either be written in camel case or in uppercase with underscores */
+        funcscope:false,  /* option suppresses warnings about declaring variables inside of control structures while accessing them later from the outside. */
+        latedef:true,     /* option prohibits the use of a variable before it was defined. */
+        nonbsp:true,      /* option warns about "non-breaking whitespace" characters. */
+        debug:false,      /* suppresses warnings about the debugger statements in your code. */
+        eqnull:false      /* option suppresses warnings about == null comparisons. */
+        
+        
         
         //reporterOutput: 'jshint.html'
           //reporter: require('checkstyle')
